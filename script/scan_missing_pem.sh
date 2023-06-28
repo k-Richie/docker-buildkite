365b60977122664b5e913cbd5dd9b53a5b5df556
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-28 17:41:12 +0530

    Update scan_missing_pem.sh

--- script/scan_missing_pem.sh
+++ script/scan_missing_pem.sh
@@ -4,10 +4,13 @@ REPO_PATH="/home/rachana/buildkite/docker-buildkite"
 
 check_missing_pem() {
   file="$1"
+  file_name=$(basename "$file")
   
-  if [[ ! -f "$file" ]]; then
-    echo "Missing .pem file: $file"
-    return 1
+  if [[ "$file_name" == *.pem ]]; then
+    if [[ ! -f "$file" ]]; then
+      echo "Missing .pem file: $file"
+      return 1
+    fi
   fi
   
   return 0
@@ -15,9 +18,7 @@ check_missing_pem() {
 
 scan_repository() {
   while IFS= read -r -d '' file; do
-    if [[ "$file" == *.pem ]]; then
-      check_missing_pem "$file"
-    fi
+    check_missing_pem "$file"
   done < <(find "$REPO_PATH" -type f -print0)
 }
 
@@ -25,3 +26,4 @@ scan_repository
 
 exit_status=$?
 exit "$exit_status"
+
