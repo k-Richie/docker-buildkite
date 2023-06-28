1fe1474e5beedc77ed09e517a4e43852b7d9da4f
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-28 10:36:08 +0530

    Create scan_permissions.sh

---
+++ script/scan_permissions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO_PATH="/path/to/repository"
+
+BAD_PERMISSIONS=("777" "666")
+
+check_file_permission() {
+  file="$1"
+  permission="$(stat -c "%a" "$file")"
+  
+  for bad_perm in "${BAD_PERMISSIONS[@]}"; do
+    if [[ "$permission" == "$bad_perm" ]]; then
+      echo "Bad permission found: $file"
+      return 1
+    fi
+  done
+  
+  return 0
+}
+
+scan_repository() {
+  while IFS= read -r -d '' file; do
+    check_file_permission "$file"
+  done < <(find "$REPO_PATH" -type f -print0)
+}
+
+scan_repository
+
+exit_status=$?
+exit "$exit_status"
