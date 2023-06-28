a5f32352472a1dafacd6153c2eb461f8f1f1c67e
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-28 11:00:45 +0530

    Create scan_missing_keys.sh

---
+++ script/scan_missing_keys.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REPO_PATH="/path/to/repository"
+
+check_missing_keys() {
+  file="$1"
+  
+  if ! grep -qE "access_key|secret_key" "$file"; then
+    echo "Missing access key or secret key in file: $file"
+    return 1
+  fi
+  
+  return 0
+}
+
+scan_repository() {
+  while IFS= read -r -d '' file; do
+    check_missing_keys "$file"
+  done < <(find "$REPO_PATH" -type f -print0)
+}
+
+scan_repository
+
+exit_status=$?
+exit "$exit_status"
