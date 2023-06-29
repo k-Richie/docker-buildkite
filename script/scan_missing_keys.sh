d303e6cde2640d3e51533b004c9968af8b87194e
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-29 15:09:31 +0530

    Update scan_missing_keys.sh

--- script/scan_missing_keys.sh
+++ script/scan_missing_keys.sh
@@ -1,25 +1,37 @@
-#!/bin/bash
+steps:
+  - label: "Scan File for Missing Access Key and Secret Key"
+    command: |
+      #!/bin/bash
 
-REPO_PATH="/home/rachana/buildkite/docker-buildkite"
+      FILE_PATH="/path/to/your/file"
 
-check_missing_keys() {
-  file="$1"
-  
-  if ! grep -qE "access_key|secret_key" "$file"; then
-    echo "Missing access key or secret key in file: $file"
-    return 1
-  fi
-  
-  return 0
-}
-#Function to scan repository
-scan_repository() {
-  while IFS= read -r -d '' file; do
-    check_missing_keys "$file"
-  done < <(find "$REPO_PATH" -type f -print0)
-}
+      check_file() {
+        local file="$1"
 
-scan_repository
+        # Check if file is empty
+        if [[ ! -s "$file" ]]; then
+          echo "File is empty: $file"
+          return 1
+        fi
 
-exit_status=$?
+        # Check for missing access key and secret key
+        if ! grep -qE "access_key\s*=\s*" "$file" || ! grep -qE "secret_key\s*=\s*" "$file"; then
+          echo "Missing access key or secret key: $file"
+          return 1
+        fi
 
+        # Check for incorrect indentation
+        if grep -qE "^\s*access_key\s*=\s*" "$file" || grep -qE "^\s*secret_key\s*=\s*" "$file"; then
+          echo "Incorrect indentation: $file"
+          return 1
+        fi
+
+        return 0
+      }
+
+      check_file "$FILE_PATH"
+      
+      exit_status=$?
+      exit "$exit_status"
+    agents:
+      queue: "your-buildkite-queue"
