e51823ab005b728a8723abb6d32f75ec9206e3ba
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-29 15:17:23 +0530

    Update scan_missing_keys.sh

--- script/scan_missing_keys.sh
+++ script/scan_missing_keys.sh
@@ -1,51 +1,30 @@
-      #!/bin/bash
+check_file() {
+  local file="$1"
+  local has_issue=false
 
-      REPO_PATH="/home/rachana/buildkite/docker-buildkite"
+  # Check if file is empty
+  if [[ ! -s "$file" ]]; then
+    echo "File is empty: $file"
+    has_issue=true
+  fi
 
-      check_file() {
-        local file="$1"
+  # Check for missing access key and secret key
+  if ! grep -qE "access_key\s*=\s*" "$file" || ! grep -qE "secret_key\s*=\s*" "$file"; then
+    echo "Missing access key or secret key: $file"
+    has_issue=true
+  fi
 
-        # Check if file is empty
-        if [[ ! -s "$file" ]]; then
-          echo "File is empty: $file"
-          return 1
-        fi
+  # Check for incorrect indentation
+  if grep -qE "^\s*access_key\s*=\s*" "$file" || grep -qE "^\s*secret_key\s*=\s*" "$file"; then
+    echo "Incorrect indentation: $file"
+    has_issue=true
+  fi
 
-        # Check for missing access key and secret key
-        if ! grep -qE "access_key\s*=\s*" "$file" || ! grep -qE "secret_key\s*=\s*" "$file"; then
-          echo "Missing access key or secret key: $file"
-          return 1
-        fi
+  if [[ "$has_issue" = true ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
 
-        # Check for incorrect indentation
-        if grep -qE "^\s*access_key\s*=\s*" "$file" || grep -qE "^\s*secret_key\s*=\s*" "$file"; then
-          echo "Incorrect indentation: $file"
-          return 1
-        fi
-
-        return 0
-      }
-
-      scan_repository() {
-        local has_issues=false
-
-        while IFS= read -r -d '' file; do
-          if ! check_file "$file"; then
-            has_issues=true
-          fi
-        done < <(find "$REPO_PATH" -type f -name "*.txt" -print0)
-
-        if [[ "$has_issues" = true ]]; then
-          return 1
-        else
-          return 0
-        fi
-      }
-
-      scan_repository
-
-      exit_status=$?
-      exit "$exit_status"
-    agents:
-      queue: "your-buildkite-queue"
 
