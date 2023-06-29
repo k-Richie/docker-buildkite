3d3bce84ca27543e3162fc5ad3427bb912b1b46e
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-06-29 18:04:01 +0530

    Update scan_missing_keys.sh

--- script/scan_missing_keys.sh
+++ script/scan_missing_keys.sh
@@ -4,15 +4,11 @@ REPO_PATH="/home/rachana/buildkite/docker-buildkite"
 
 echo "Searching for files containing Access_key..."
 
-for file in "$REPO_PATH"/*; do
-  if [[ -f "$file" ]]; then
-    if grep -q "Access_key" "$file"; then
-      if grep -qE '^\s*Access_key\s*=' "$file"; then
-        echo "File contains Access_key with incorrect indentation: $file"
-      else
-        echo "File contains Access_key with correct indentation: $file"
-      fi
-    fi
+find "$REPO_PATH" -type f -exec grep -H "Access_key" {} + | while IFS=: read -r file line; do
+  if grep -qE '^\s*Access_key\s*=' "$file"; then
+    echo "File contains Access_key with correct indentation: $file"
+  else
+    echo "File contains Access_key with incorrect indentation: $file"
   fi
 done
 
